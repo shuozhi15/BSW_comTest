59a9caeee280f3ec022d50b8019409e4dbd4dfaf
Wenqi Cai <w.cai@gotion.com>
2021-11-29 10:13:56 +0800

    rte1

--- src/bsw/CodeGen_Version.h
+++ src/bsw/CodeGen_Version.h
@@ -8,6 +8,6 @@
 #define CODEGEN_LICENSED_STACKS "Common, RTA-BASE, RTA-CAN, RTA-COM, RTA-DIAG, RTA-HWD(Can), RTA-J1939, RTA-MEM, RTA-SAFE, RTA-XCP"
 #define CODEGEN_CONFIGURED_MODULES "ComStack, CanIf, CanNm, CanSM, Com, ComM, Nm, PduR"
 #define CODEGEN_SELECTED_MODULES "Rba_ArxmlGen, CanIf, Com, CanNm, CanSM, PduR, CanNm_PreCompile_and_PB_Variant, ComM, ComStack, Nm"
-#define CODEGEN_GENERATED_ON "2021-11-25T18:25:19.433+08:00[Asia/Shanghai]"
+#define CODEGEN_GENERATED_ON "2021-11-29T10:12:00.812+08:00[Asia/Shanghai]"
 
 #endif
