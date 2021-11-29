a29fc5f13f7572519d40afdc0bd13c81c2f75c63
Wenqi Cai <w.cai@gotion.com>
2021-11-29 11:30:57 +0800

    add ECUM

--- src/bsw/CodeGen_Version.h
+++ src/bsw/CodeGen_Version.h
@@ -7,7 +7,7 @@
 #define CODEGEN_ISOLAR_AB_VERSION "ISOLAR-A/B 7.0.1"
 #define CODEGEN_LICENSED_STACKS "Common, RTA-BASE, RTA-CAN, RTA-COM, RTA-DIAG, RTA-HWD(Can), RTA-J1939, RTA-MEM, RTA-SAFE, RTA-XCP"
 #define CODEGEN_CONFIGURED_MODULES "ComStack, CanIf, CanNm, CanSM, Com, ComM, Nm, PduR"
-#define CODEGEN_SELECTED_MODULES "Rba_ArxmlGen, CanIf, Com, CanNm, CanSM, PduR, CanNm_PreCompile_and_PB_Variant, ComM, ComStack, Nm"
-#define CODEGEN_GENERATED_ON "2021-11-29T10:12:00.812+08:00[Asia/Shanghai]"
+#define CODEGEN_SELECTED_MODULES "Rba_ArxmlGen, CanIf, Com, CanNm, CanSM, EcuM, PduR, CanNm_PreCompile_and_PB_Variant, ComM, ComStack, Nm"
+#define CODEGEN_GENERATED_ON "2021-11-29T11:30:07.241+08:00[Asia/Shanghai]"
 
 #endif
